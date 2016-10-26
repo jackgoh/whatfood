4509bb32da03decfb0f704e9576507e13b1c4f09
Jack goh <jackgoh0@gmail.com>
2016-10-26 05:15:06 -0400

    Added data agmentation

--- deep/test.py
+++ deep/test.py
@@ -23,7 +23,7 @@ from keras.utils import np_utils
 batch_size = 32
 nb_classes = 10
 nb_epoch = 200
-data_augmentation = True
+data_augmentation = False
 
 # input image dimensions
 img_rows, img_cols = 32, 32
@@ -41,7 +41,6 @@ Y_train = np_utils.to_categorical(y_train, nb_classes)
 Y_test = np_utils.to_categorical(y_test, nb_classes)
 
 model = Sequential()
-
 model.add(Convolution2D(32, 3, 3, border_mode='same',
                         input_shape=X_train.shape[1:]))
 model.add(Activation('relu'))
@@ -75,7 +74,7 @@ X_test = X_test.astype('float32')
 X_train /= 255
 X_test /= 255
 
-print (X_train.shape[1:])
+print (X_train.shape)
 
 if not data_augmentation:
     print('Not using data augmentation.')
