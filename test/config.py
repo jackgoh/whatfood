345aed009c422b0f28a846a933749c6d5c1a6b7a
Jack goh <jackgoh0@gmail.com>
2016-12-10 23:12:13 +0800

    ignore .py~ file

--- test/config.py
+++ test/config.py
@@ -1,2 +1,9 @@
 nb_class = 10
+n_fold = 2
+
 data_path = "../dataset/myfood4-224.hkl"
+weights_path = '../dataset/alexnet_weights.h5'
+top_model_weights_path = 'bottleneck_fc_model.h5'
+
+classes = ['AisKacang' , 'AngKuKueh' , 'ApamBalik' , 'Asamlaksa' , 'Bahulu' , 'Bakkukteh',
+         'BananaLeafRice' , 'Bazhang' , 'BeefRendang' , 'BingkaUbi']
