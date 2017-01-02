9593dbc342e70932cd9fa5116e6bc82b2736b329
Jack goh <jackgoh0@gmail.com>
2017-01-02 21:05:02 +0800

    Added train scratch file

--- deep/util.py
+++ deep/util.py
@@ -546,6 +546,55 @@ def load_svm_deep_features_model(nb_class, weights_path=None, top_model_weight_p
 
     return model
 
+def load_alex_model(weights_path=None, nb_class=None):
+
+    inputs = Input(shape=(3,227,227))
+
+    conv_1 = Convolution2D(96, 11, 11,subsample=(4,4),activation='relu',
+                           name='conv_1')(inputs)
+
+    conv_2 = MaxPooling2D((3, 3), strides=(2,2))(conv_1)
+    conv_2 = crosschannelnormalization(name="convpool_1")(conv_2)
+    conv_2 = ZeroPadding2D((2,2))(conv_2)
+    conv_2 = merge([
+        Convolution2D(128,5,5,activation="relu",name='conv_2_'+str(i+1))(
+            splittensor(ratio_split=2,id_split=i)(conv_2)
+        ) for i in range(2)], mode='concat',concat_axis=1,name="conv_2")
+
+    conv_3 = MaxPooling2D((3, 3), strides=(2, 2))(conv_2)
+    conv_3 = crosschannelnormalization()(conv_3)
+    conv_3 = ZeroPadding2D((1,1))(conv_3)
+    conv_3 = Convolution2D(384,3,3,activation='relu',name='conv_3')(conv_3)
+
+    conv_4 = ZeroPadding2D((1,1))(conv_3)
+    conv_4 = merge([
+        Convolution2D(192,3,3,activation="relu",name='conv_4_'+str(i+1))(
+            splittensor(ratio_split=2,id_split=i)(conv_4)
+        ) for i in range(2)], mode='concat',concat_axis=1,name="conv_4")
+
+    conv_5 = ZeroPadding2D((1,1))(conv_4)
+    conv_5 = merge([
+        Convolution2D(128,3,3,activation="relu",name='conv_5_'+str(i+1))(
+            splittensor(ratio_split=2,id_split=i)(conv_5)
+        ) for i in range(2)], mode='concat',concat_axis=1,name="conv_5")
+
+    conv_5 = MaxPooling2D((3, 3), strides=(2,2),name="convpool_5")(conv_5)
+    dense_1 = Flatten(name="flatten")(conv_5)
+    dense_1 = Dense(4096, activation='relu',name='dense_1')(dense_1)
+    dense_2 = Dropout(0.5)(dense_1)
+    dense_2 = Dense(4096, activation='relu',name='dense_2')(dense_2)
+    dense_3 = Dropout(0.5)(dense_2)
+    dense_3 = Dense(nb_class,name='dense_3')(dense_3)
+    prediction = Activation("softmax",name="softmax")(dense_3)
+
+
+    base_model = Model(input=inputs, output=prediction)
+
+    if weights_path:
+        base_model.load_weights(weights_path)
+        
+    return base_model
+
 def load_svm_alex_model(weights_path=None, nb_class=None):
 
     inputs = Input(shape=(3,227,227))
