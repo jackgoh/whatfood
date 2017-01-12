3bc313092fe5d59f01418701384a2f4d569498ca
Jack goh <jackgoh0@gmail.com>
2017-01-12 10:54:20 +0800

    Fix categorial problem

--- hand-crafted/colorHistogram.py
+++ hand-crafted/colorHistogram.py
@@ -4,6 +4,10 @@
 # import the necessary packages
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.metrics import confusion_matrix
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
 from imutils import paths
 from sklearn import svm
 import numpy as np
@@ -11,6 +15,59 @@ import argparse
 import imutils
 import cv2
 import os
+
+def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.jet):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(100)
+    plt.xticks(tick_marks, ['AisKacang' , 'AngKuKueh' , 'ApamBalik' , 'Asamlaksa' , 'Bahulu' , 'Bakkukteh',
+ 'BananaLeafRice' , 'Bazhang' , 'BeefRendang' , 'BingkaUbi' , 'Buburchacha',
+ 'Buburpedas' , 'Capati' , 'Cendol' , 'ChaiTowKuay' , 'CharKuehTiao' , 'CharSiu',
+ 'CheeCheongFun' , 'ChiliCrab' , 'Chweekueh' , 'ClayPotRice' , 'CucurUdang',
+ 'CurryLaksa' , 'CurryPuff' , 'Dodol' , 'Durian' , 'DurianCrepe' , 'FishHeadCurry',
+ 'Guava' , 'HainaneseChickenRice' , 'HokkienMee' , 'Huatkuih' , 'IkanBakar',
+ 'Kangkung' , 'KayaToast' , 'Keklapis' , 'Ketupat' , 'KuihDadar' , 'KuihLapis',
+ 'KuihSeriMuka' , 'Langsat' , 'Lekor' , 'Lemang' , 'LepatPisang' , 'LorMee',
+ 'Maggi goreng' , 'Mangosteen' , 'MeeGoreng' , 'MeeHoonKueh' , 'MeeHoonSoup',
+ 'MeeJawa' , 'MeeRebus' , 'MeeRojak' , 'MeeSiam' , 'Murtabak' , 'Murukku',
+ 'NasiGorengKampung' , 'NasiImpit' , 'Nasikandar' , 'Nasilemak' , 'Nasipattaya',
+ 'Ondehondeh' , 'Otakotak' , 'OysterOmelette' , 'PanMee' , 'PineappleTart',
+ 'PisangGoreng' , 'Popiah' , 'PrawnMee' , 'Prawnsambal' , 'Puri' , 'PutuMayam',
+ 'PutuPiring' , 'Rambutan' , 'Rojak' , 'RotiCanai' , 'RotiJala' , 'RotiJohn',
+ 'RotiNaan' , 'RotiTissue' , 'SambalPetai' , 'SambalUdang' , 'Satay' , 'Sataycelup',
+ 'SeriMuka' , 'SotoAyam' , 'TandooriChicken' , 'TangYuan' , 'TauFooFah',
+ 'TauhuSumbat' , 'Thosai' , 'TomYumSoup' , 'Wajik' , 'WanTanMee' , 'WaTanHo' , 'Wonton',
+ 'YamCake' , 'YongTauFu' , 'Youtiao' , 'Yusheng'], rotation=45)
+    plt.yticks(tick_marks, ['AisKacang' , 'AngKuKueh' , 'ApamBalik' , 'Asamlaksa' , 'Bahulu' , 'Bakkukteh',
+ 'BananaLeafRice' , 'Bazhang' , 'BeefRendang' , 'BingkaUbi' , 'Buburchacha',
+ 'Buburpedas' , 'Capati' , 'Cendol' , 'ChaiTowKuay' , 'CharKuehTiao' , 'CharSiu',
+ 'CheeCheongFun' , 'ChiliCrab' , 'Chweekueh' , 'ClayPotRice' , 'CucurUdang',
+ 'CurryLaksa' , 'CurryPuff' , 'Dodol' , 'Durian' , 'DurianCrepe' , 'FishHeadCurry',
+ 'Guava' , 'HainaneseChickenRice' , 'HokkienMee' , 'Huatkuih' , 'IkanBakar',
+ 'Kangkung' , 'KayaToast' , 'Keklapis' , 'Ketupat' , 'KuihDadar' , 'KuihLapis',
+ 'KuihSeriMuka' , 'Langsat' , 'Lekor' , 'Lemang' , 'LepatPisang' , 'LorMee',
+ 'Maggi goreng' , 'Mangosteen' , 'MeeGoreng' , 'MeeHoonKueh' , 'MeeHoonSoup',
+ 'MeeJawa' , 'MeeRebus' , 'MeeRojak' , 'MeeSiam' , 'Murtabak' , 'Murukku',
+ 'NasiGorengKampung' , 'NasiImpit' , 'Nasikandar' , 'Nasilemak' , 'Nasipattaya',
+ 'Ondehondeh' , 'Otakotak' , 'OysterOmelette' , 'PanMee' , 'PineappleTart',
+ 'PisangGoreng' , 'Popiah' , 'PrawnMee' , 'Prawnsambal' , 'Puri' , 'PutuMayam',
+ 'PutuPiring' , 'Rambutan' , 'Rojak' , 'RotiCanai' , 'RotiJala' , 'RotiJohn',
+ 'RotiNaan' , 'RotiTissue' , 'SambalPetai' , 'SambalUdang' , 'Satay' , 'Sataycelup',
+ 'SeriMuka' , 'SotoAyam' , 'TandooriChicken' , 'TangYuan' , 'TauFooFah',
+ 'TauhuSumbat' , 'Thosai' , 'TomYumSoup' , 'Wajik' , 'WanTanMee' , 'WaTanHo' , 'Wonton',
+ 'YamCake' , 'YongTauFu' , 'Youtiao' , 'Yusheng'])
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+def autolabel(rects):
+    # attach some text labels
+    for rect in rects:
+        height = rect.get_height()
+        plt.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                '%d' % int(height),
+                ha='center', va='bottom')
 
 def image_to_feature_vector(image, size=(32, 32)):
 	# resize the image to a fixed size, then flatten the image into
@@ -63,52 +120,145 @@ for (i, imagePath) in enumerate(imagePaths):
 	imagePath = imagePath.replace('\\', '')
 
 	image = cv2.imread(imagePath)
-	label = imagePath.split(os.path.sep)[-1].split("(")[0]
-
+	label = imagePath.split(os.path.sep)[-1].split("(")[0].replace(' ', '')
 	# extract raw pixel intensity "features", followed by a color
 	# histogram to characterize the color distribution of the pixels
 	# in the image
-	pixels = image_to_feature_vector(image)
+	#pixels = image_to_feature_vector(image)
 	hist = extract_color_histogram(image)
 
 	# update the raw images, features, and labels matricies,
 	# respectively
-	rawImages.append(pixels)
+	#rawImages.append(pixels)
 	features.append(hist)
 	labels.append(label)
 
-	# show an update every 1,000 images
-	if i > 0 and i % 1000 == 0:
+	# show an update every 200 images
+	if i > 0 and i % 200 == 0:
 		print("[INFO] processed {}/{}".format(i, len(imagePaths)))
 
-# show some information on the memory consumed by the raw images
-# matrix and features matrix
-rawImages = np.array(rawImages)
+
 features = np.array(features)
 labels = np.array(labels)
-print("[INFO] pixels matrix: {:.2f}MB".format(
-	rawImages.nbytes / (1024 * 1000.0)))
+
+le = preprocessing.LabelEncoder()
+le.fit(labels)
+labels = le.transform(labels)
+print labels
+
 print("[INFO] features matrix: {:.2f}MB".format(
 	features.nbytes / (1024 * 1000.0)))
 
-# partition the data into training and testing splits, using 75%
-# of the data for training and the remaining 25% for testing
-(trainRI, testRI, trainRL, testRL) = train_test_split(
-	rawImages, labels, test_size=0.25, random_state=42)
-(trainFeat, testFeat, trainLabels, testLabels) = train_test_split(
-	features, labels, test_size=0.25, random_state=42)
 
-# train and evaluate a svm classifer on the raw pixel intensities
-print("[INFO] evaluating raw pixel accuracy...")
-model = svm.SVC(kernel='linear', probability=True)
-model.fit(trainRI, trainRL)
-acc = model.score(testRI, testRL)
-print("[INFO] raw pixel accuracy: {:.2f}%".format(acc * 100))
 
-# train and evaluate a svm classifer on the histogram
-# representations
-print("[INFO] evaluating histogram accuracy...")
-model = svm.SVC(kernel='linear', probability=True)
-model.fit(trainFeat, trainLabels)
-acc = model.score(testFeat, testLabels)
-print("[INFO] histogram accuracy: {:.2f}%".format(acc * 100))
+# Training
+k = 2
+sfold = StratifiedKFold(labels, n_folds=k)
+
+total_score = 0
+fold_count = 1
+
+for train_index, test_index in sfold:
+    print "Training SVM.."
+    train_data, test_data = features[train_index], features[test_index]
+    train_label, test_label = labels[train_index], labels[test_index]
+
+    clf = svm.SVC(kernel='linear', C=1.0, probability=True)
+    clf.fit(train_data, train_label)
+    y_pred = clf.predict(test_data)
+
+    # Compute confusion matrix
+    cm = confusion_matrix(test_label, y_pred)
+    np.set_printoptions(precision=2)
+    #print('Confusion matrix, without normalization')
+    #print(cm)
+    plt.figure()
+    plot_confusion_matrix(cm)
+
+    # Normalize the confusion matrix by row (i.e by the number of samples
+    # in each class)
+    cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    #print('Normalized confusion matrix')
+    #print(cm_normalicd pzed)
+    plt.figure()
+    plot_confusion_matrix(cm_normalized, title='Normalized confusion matrix')
+
+    plt.show()
+
+
+    score = clf.score(test_data,test_label)
+    print "Acc for fold ", fold_count, "= ", score
+
+    scores = clf.predict_proba(test_data)
+    n = 5
+    indices = np.argsort(scores)[:,:-n-1:-1]
+
+    # Get accuracy
+    top1 = 0.0
+    top5 = 0.0
+
+    correct_predict_top1 = np.zeros((100,), dtype=np.int)
+    correct_predict_top5 = np.zeros((100,), dtype=np.int)
+
+    for image_index, index_list in enumerate(indices):
+        if test_label[image_index] == index_list[0]:
+            top1 += 1.0
+        if test_label[image_index] in index_list:
+            top5 += 1.0
+
+    image_index = None
+    index_list = None
+    start_index = 0
+    end_index = 99
+
+    for class_label in range(0,100):
+        for image_index in range(start_index,end_index+1):
+            if test_label[image_index] == indices[image_index][0]:
+                correct_predict_top1[class_label] += 1
+            if test_label[image_index] in indices[image_index]:
+                correct_predict_top5[class_label] += 1
+        start_index += 100
+        end_index += 100
+
+    print correct_predict_top1
+    print correct_predict_top5
+
+    objects = ['AisKacang' , 'AngKuKueh' , 'ApamBalik' , 'Asamlaksa' , 'Bahulu' , 'Bakkukteh',
+     'BananaLeafRice' , 'Bazhang' , 'BeefRendang' , 'BingkaUbi' , 'Buburchacha',
+     'Buburpedas' , 'Capati' , 'Cendol' , 'ChaiTowKuay' , 'CharKuehTiao' , 'CharSiu',
+     'CheeCheongFun' , 'ChiliCrab' , 'Chweekueh' , 'ClayPotRice' , 'CucurUdang',
+     'CurryLaksa' , 'CurryPuff' , 'Dodol' , 'Durian' , 'DurianCrepe' , 'FishHeadCurry',
+     'Guava' , 'HainaneseChickenRice' , 'HokkienMee' , 'Huatkuih' , 'IkanBakar',
+     'Kangkung' , 'KayaToast' , 'Keklapis' , 'Ketupat' , 'KuihDadar' , 'KuihLapis',
+     'KuihSeriMuka' , 'Langsat' , 'Lekor' , 'Lemang' , 'LepatPisang' , 'LorMee',
+     'Maggi goreng' , 'Mangosteen' , 'MeeGoreng' , 'MeeHoonKueh' , 'MeeHoonSoup',
+     'MeeJawa' , 'MeeRebus' , 'MeeRojak' , 'MeeSiam' , 'Murtabak' , 'Murukku',
+     'NasiGorengKampung' , 'NasiImpit' , 'Nasikandar' , 'Nasilemak' , 'Nasipattaya',
+     'Ondehondeh' , 'Otakotak' , 'OysterOmelette' , 'PanMee' , 'PineappleTart',
+     'PisangGoreng' , 'Popiah' , 'PrawnMee' , 'Prawnsambal' , 'Puri' , 'PutuMayam',
+     'PutuPiring' , 'Rambutan' , 'Rojak' , 'RotiCanai' , 'RotiJala' , 'RotiJohn',
+     'RotiNaan' , 'RotiTissue' , 'SambalPetai' , 'SambalUdang' , 'Satay' , 'Sataycelup',
+     'SeriMuka' , 'SotoAyam' , 'TandooriChicken' , 'TangYuan' , 'TauFooFah',
+     'TauhuSumbat' , 'Thosai' , 'TomYumSoup' , 'Wajik' , 'WanTanMee' , 'WaTanHo' , 'Wonton',
+     'YamCake' , 'YongTauFu' , 'Youtiao' , 'Yusheng']
+    y_pos = np.arange(len(objects))
+    performance = correct_predict_top1
+
+    rects1 = plt.bar(y_pos, performance)
+    plt.xticks(y_pos, objects, rotation='vertical')
+    plt.ylabel('Total true positive')
+    plt.title('Total true positive per sample')
+
+    autolabel(rects1)
+    plt.savefig('barchart_deep_feaures'+ str(fold_count) +'.png')
+    plt.show()
+
+    print correct_predict_top1
+    print correct_predict_top5
+
+    print('Top-1 Accuracy: ' + str(top1 / len(test_label) * 100.0) + '%')
+    print('Top-5 Accuracy: ' + str(top5 / len(test_label) * 100.0) + '%')
+    total_score = total_score + top1 / len(test_label)
+    fold_count = fold_count + 1
+
+print "Accuracy : ", total_score/k
